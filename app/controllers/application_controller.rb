3bd7020db33e47b135ec82a19ffbeba18ef108cf
Mai Vang <vmaineng@gmail.com>
2022-06-17 15:08:14 -0700

    commit tree 3b260c83acbcb75a2e7a42a8539eaf6a40672766

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < Sinatra::Base
 
   # add routes
 
+  class ApplicationController
+    get '/bakeries' do
+      bakeries = Bakery.all
+      bakeries.to_json
+    end
+  end
+
 end
